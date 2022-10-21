483cc50976ed8860c4e11f30965831dd68c4bdf4
nirmalkumar63 <nirmalkumar.palanivel@indiumsoft.com>
2022-10-21 12:16:28 +0530

    kk

---
+++ src/components/Employee.js
@@ -0,0 +1,21 @@
+const Employee = [
+
+
+    {
+        id:"1",
+        Name:"sam",
+        Age:"28"
+
+    },{
+        id:"2",
+        Name:"john",
+        Age:"29"
+ 
+    },{
+        id:"1",
+        Name:"tony",
+        Age:"28"
+    
+    }
+]
+export default Employee;
